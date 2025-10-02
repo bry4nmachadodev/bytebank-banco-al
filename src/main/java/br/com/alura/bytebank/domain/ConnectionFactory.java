5fb3a63fe8f9eb29c924c128a258e5439f6026f5
Bryan Machado <bryanmachado1703@icloud.com>
2025-10-02 14:15:10 -0300

    isolando método para evitar duplicação de código.

--- src/main/java/br/com/alura/bytebank/domain/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/domain/ConnectionFactory.java
@@ -4,18 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class ConexaoDB {
+public class ConnectionFactory {
 
-    public static void main(String ...x){
+    public Connection recuperarConexao(){
         try {
-            Connection connection = DriverManager
+            return DriverManager
                     .getConnection("jdbc:mysql://localhost:3306/byte_bank?user=root&password=bryan2009");
-
-            System.out.println("Recuperei a conexão");
-
-            connection.close();
         } catch (SQLException e) {
-            System.out.println(e);
+            throw new RuntimeException(e);
         }
     }
+
 }
